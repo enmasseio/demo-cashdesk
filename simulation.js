5b0b8f7e2e6d55f654b9f37dd6c323ae233ed953
jos <wjosdejong@gmail.com>
2014-08-13 11:05:32 +0200

    Created a more decent Logger

--- simulation.js
+++ simulation.js
@@ -1,24 +1,39 @@
 var actors = require('simple-actors');
 var distribus = require('distribus');
+var hypertimer = require('hypertimer');
 
-var timer = require('./lib/timer');
+var Logger = require('./lib/Logger');
 var CashDesk = require('./lib/CashDesk');
 var Cashier = require('./lib/Cashier');
 var Customer = require('./lib/Customer');
 var Supermarket = require('./lib/Supermarket');
+
+var timer = hypertimer({rate: 'discrete'});
+
+var logger = new Logger({
+  realTime: function () {
+    return new Date().toISOString();
+  },
+  simTime: function () {
+    return timer.getTime().toISOString();
+  }
+});
+logger.on('log', function (log) {
+  console.log(JSON.stringify(log))
+});
 
 var host = new distribus.Host();
 var messagebus = new actors.DistribusMessageBus({
   host: host
 });
 
-var supermarket = new Supermarket('Super & co', {cashDesks: 4});
+var supermarket = new Supermarket('Super & co', {logger: logger, cashDesks: 4});
 
-var cashier = new Cashier('Tamara');
+var cashier = new Cashier('Tamara', {logger: logger, timer: timer});
 cashier.connect(messagebus);
 cashier.open(supermarket.cashDesks[0]);
 
-var customer = new Customer('Thomas');
+var customer = new Customer('Thomas', {logger: logger, timer: timer});
 customer.connect(messagebus);
 
 customer.planShopping(supermarket);
