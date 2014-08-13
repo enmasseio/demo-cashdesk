8c45e68eca8f88835c5d78f8d2e83d346eec4f6f
jos <wjosdejong@gmail.com>
2014-08-13 10:30:11 +0200

    Changed Cashier and Customer into babblers

--- simulation.js
+++ simulation.js
@@ -1,15 +1,24 @@
-var moment = require('moment');
+var actors = require('simple-actors');
+var distribus = require('distribus');
+
 var timer = require('./lib/timer');
 var CashDesk = require('./lib/CashDesk');
 var Cashier = require('./lib/Cashier');
 var Customer = require('./lib/Customer');
 var Supermarket = require('./lib/Supermarket');
 
+var host = new distribus.Host();
+var messagebus = new actors.DistribusMessageBus({
+  host: host
+});
+
 var supermarket = new Supermarket('Super & co', {cashDesks: 4});
 
 var cashier = new Cashier('Tamara');
+cashier.connect(messagebus);
 cashier.open(supermarket.cashDesks[0]);
 
 var customer = new Customer('Thomas');
+customer.connect(messagebus);
 
 customer.planShopping(supermarket);
