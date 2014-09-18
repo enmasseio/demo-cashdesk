41d4d273ec37b8831f283403e38b4a26a56db0ea
jos <wjosdejong@gmail.com>
2014-09-18 09:26:15 +0200

    Adding timer and logger to eve.system

--- simulation.js
+++ simulation.js
@@ -1,5 +1,4 @@
 var eve = require('evejs');
-var distribus = require('distribus');
 var hypertimer = require('hypertimer');
 
 var Logger = require('./lib/Logger');
@@ -8,29 +7,31 @@ var Cashier = require('./lib/Cashier');
 var Customer = require('./lib/Customer');
 var Supermarket = require('./lib/Supermarket');
 
-var timer = hypertimer({rate: 'discrete'});
+// TODO: use default eve.system.timer as soon as it's implemented in eve itself
+eve.system.timer = hypertimer({rate: 'discrete'});
 
-var logger = new Logger({
+// TODO: use default eve.system.logger as soon as eve supports logging itself
+eve.system.logger = new Logger({
   realTime: function () {
     return new Date().toISOString();
   },
   simTime: function () {
-    return timer.getTime().toISOString();
+    return eve.system.timer.getTime().toISOString();
   }
 });
 
-var supermarket = new Supermarket('Super & co', {logger: logger, cashDesks: 4});
+var supermarket = new Supermarket('Super & co', {cashDesks: 4});
 
-var cashier = new Cashier('Tamara', {logger: logger, timer: timer});
+var cashier = new Cashier('Tamara');
 cashier.open(supermarket.cashDesks[0]);
 
-var customer = new Customer('Thomas', {logger: logger, timer: timer});
+var customer = new Customer('Thomas');
 
 /**
  * Start the simulation
  */
 exports.start = function () {
-  logger.log({event: 'start'});
+  eve.system.logger.log({event: 'start'});
 
   customer.planShopping(supermarket);
 };
@@ -40,12 +41,12 @@ exports.start = function () {
  * @param {string} event Available events: 'log'
  * @param {function} callback
  */
-exports.on = logger.on.bind(logger);
+exports.on = eve.system.logger.on.bind(eve.system.logger);
 
 /**
  * Retrieve all logs
  * @returns {Array}
  */
 exports.logs = function () {
-  return logger.logs;
+  return eve.system.logger.logs;
 };
